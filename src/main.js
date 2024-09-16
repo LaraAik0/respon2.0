ea106d27330f7bf0e1a53585700a9007e225789a
Lara_Aiko <larasagaiif@gmail.com>
2024-09-16 10:31:16 -0300

    Add files via upload

--- src/main.js
+++ src/main.js
@@ -5,3 +5,11 @@ import router from './router';
 const app = createApp(App);
 app.use(router);
 app.mount('#app');
+
+// main.js
+import Vue from 'vue';
+import VueLazyload from 'vue-lazyload';
+
+Vue.use(VueLazyload, {
+  loading: 'path/to/loading-image.png'
+});
