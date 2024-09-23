62d0246a7a641218552d7f3f69c046f3b6c088b2
Lara_Aiko <larasagaiif@gmail.com>
2024-09-23 09:13:05 -0300

    Add files via upload

--- src/components/imagemDiversos.vue
+++ src/components/imagemDiversos.vue
@@ -2,33 +2,40 @@
     <div>
         <div class="secao">
             <div class="subsecao">
-                <img  width="60%" class="imagemUnica"  loading="lazy" src="@/assets/a1.jpg" />
+                <div v-if="isLoading" class="skeleton"></div>
+                <img v-else width="60%" class="imagemUnica" loading="lazy" :src="require('@/assets/a1.jpg')" @load="isLoading = false" />
             </div>
         </div>
         <div class="secao">
             <div class="subsecao">
-                <img  width="60%" class="imagemNaoUnica" loading="lazy" src="@/assets/b1.jpg" />
+                <img width="60%" class="imagemNaoUnica" loading="lazy" :src="require('@/assets/b1.jpg')" />
             </div>
             <div class="subsecao">
-                <img width="60%" class="imagemNaoUnica" loading="lazy" src="@/assets/c1.jpg" />
-            </div>
-            <div class="subsecao" >
-                <img width="60%" class="imagemNaoUnica" loading="lazy" src="@/assets/d1.jpg" />
+                <img width="60%" class="imagemNaoUnica" loading="lazy" :src="require('@/assets/c1.jpg')" />
             </div>
             <div class="subsecao">
-                <img  width="60%" height="680px" loading="lazy" class="imagemNaoUnica" src="@/assets/e1.jpg" />
+                <img width="60%" class="imagemNaoUnica" loading="lazy" :src="require('@/assets/d1.jpg')" />
+            </div>
+            <div class="subsecao">
+                <img width="60%" height="680px" loading="lazy" class="imagemNaoUnica" :src="require('@/assets/e1.jpg')" />
             </div>
         </div>
     </div>
 </template>
 
 <script>
-    export default {
-        name: 'imagemDiversos',
-        props: {
-            msg: String
-        }
+export default {
+    name: 'imagemDiversos',
+    data() {
+        return {
+            isLoading: true // Inicializa como true para mostrar o skeleton
+        };
+    },
+    props: {
+        msg: String
     }
+}
 </script>
 
-<style></style>
+<style>
+</style>
--- src/main.js
+++ src/main.js
@@ -1,15 +1,14 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
-
-const app = createApp(App);
-app.use(router);
-app.mount('#app');
-
-// main.js
-import Vue from 'vue';
 import VueLazyload from 'vue-lazyload';
 
-Vue.use(VueLazyload, {
-  loading: 'path/to/loading-image.png'
+const app = createApp(App);
+
+// Usando o VueLazyload com opções
+app.use(VueLazyload, {
+  loading: 'path/to/loading-image.png',
 });
+
+app.use(router);
+app.mount('#app');
